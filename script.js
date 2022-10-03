3875c5a073735f1cd489faebe4f2244c09f60e04
xroy-destroy <xroy.ocean@gmail.com>
2022-10-02 19:24:02 -0700

    Solve task 4 - lastLetter(), add pseudocode for task 3 to script

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function add7(number) {
 function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
+
+// TASK 3
+function capitalize (string){
+  // slice(0,1) and toUpperCase()
+  // slice(1) and toLowerCase()
+  // return slicedCasedA + slicedCasedB
+} 
+
+// TASK 4
+function lastLetter(string) {
+  return string.substr(-1,1);
+}
